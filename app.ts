f3135fe1877a1127621d07595b6ce348345a15fe
Muhammad Ahmed Jamil <ahmed.jamil@koderlabs.com>
2022-08-29 16:57:36 +0500

    final commit

--- app.ts
+++ app.ts
@@ -3,57 +3,79 @@ import { scrollPageToBottom } from "puppeteer-autoscroll-down";
 import express, { Express, Request, Response } from "express";
 import cors from "cors";
 import { main } from "./download-image";
-let username = "osafda879";
-let password = "X:$Xx6eHS9JKB!m";
-
+import axios from "axios";
+let username = "";
+let password = "";
+const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
 let app: Express = express();
 let PORT = 3000;
 app.use(cors());
 app.use(express.json());
 app.post("/", async (req: Request, res: Response) => {
-  console.log("[POST] : working");
-  const url = req.body.url;
-  const browser = await launch({ headless: false });
-  const page = await browser.newPage();
-  await page.goto(url);
-  await page.waitForTimeout(5000);
   try {
-    await page.type("input[name='username']", username);
-    await page.type("input[name='password']", password);
-    await page.click("button[type='submit']");
+    console.log("[POST] : working");
+    const url = req.body.url;
+    const browser = await launch({ headless: false });
+    const page = await browser.newPage();
+    await page.goto(url);
     await page.waitForTimeout(5000);
-  } catch (err) {
-    await page.click("button[type='button']");
-    await page.waitForTimeout(5000);
-    await page.type("input[name='username']", username);
-    await page.type("input[name='password']", password);
-    await page.click("button[type='submit']");
-    await page.waitForTimeout(5000);
-  }
-  try {
-    await page.click("button[type='button']");
-    await page.waitForTimeout(5000);
-  } catch (err) {
-    // await page.click("button[type='button']");
-    await page.waitForTimeout(5000);
-  }
-  await page.waitForTimeout(15000);
+    try {
+      await page.type("input[name='username']", username);
+      await page.type("input[name='password']", password);
+      await page.click("button[type='submit']");
+      await page.waitForTimeout(5000);
+    } catch (err) {
+      await page.click("button[type='button']");
+      await page.waitForTimeout(5000);
+      await page.type("input[name='username']", username);
+      await page.type("input[name='password']", password);
+      await page.click("button[type='submit']");
+      await page.waitForTimeout(5000);
+    }
+    try {
+      await page.click("button[type='button']");
+      await page.waitForTimeout(5000);
+    } catch (err) {
+      // await page.click("button[type='button']");
+      await page.waitForTimeout(5000);
+    }
+    await page.waitForTimeout(15000);
 
-  const lastPosition = await scrollPageToBottom(page, {});
-  await page.waitForTimeout(15000);
-  let images = await page.evaluate(() => {
-    const images = document.querySelectorAll("img");
-    return [...images].map((img) => img.src);
-  });
-  console.log(images);
-  console.log(lastPosition);
-  console.log(images.length);
-  page.close();
-  browser.close();
-  await main(images);
-  return res.send({
-    data: images,
-  });
+    const lastPosition = await scrollPageToBottom(page, {});
+    await page.waitForTimeout(15000);
+    let images = await page.evaluate(() => {
+      const images = document.querySelectorAll("img");
+      return [...images].map((img) => img.src);
+    });
+    console.log(images);
+    console.log(lastPosition);
+    console.log(images.length);
+    page.close();
+    browser.close();
+    const data: string[] = await main(images);
+    console.log(data);
+    let error = 0;
+    data.forEach(async (val) => {
+      try {
+        const result = await axios.post(
+          "http://localhost:9000/create-product",
+          {
+            images: val,
+          }
+        );
+
+        await sleep(5000);
+        console.log("RESUTL [POST] : ", result.data);
+        await sleep(5000);
+      } catch (e: any) {
+        error++;
+        console.log("error", error);
+      }
+    });
+    res.send("done");
+  } catch (err) {
+    return res.status(500).send(err);
+  }
 });
 
 app.listen(PORT, () => console.log("Listening on port 3000"));
--- download-image.ts
+++ download-image.ts
@@ -1,17 +1,39 @@
 import axios from "axios";
 import fs from "fs";
 
-export const main = async (parsedImages : string[]) => {
-  for (let i = 0; i < parsedImages.length; i++) {
-    axios.get(parsedImages[i], { responseType: "stream" }).then((response) => {
-      response.data
-        .pipe(fs.createWriteStream(`./medusa-admin/public/${i}.jpg`))
-        .on("error", (err: any) => {
-          console.log(err);
-        })
-        .once("close", () => {
-          console.log("done");
+const downloadImage = (url: string, path: string)  => {
+  return new Promise((resolve, reject) => {
+    axios({
+      method: "get",
+      url,
+      responseType: "stream",
+    })
+      .then((response) => {
+        response.data.pipe(fs.createWriteStream(`./medusa-admin/public/${path}.jpg`));
+        let error = null;
+        response.data.on("end", () => {
+          resolve(path);
         });
-    });
+        response.data.on("error", (err : any) => {
+          error = err;
+          reject(err);
+        });
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  })
+}
+export const main = async (parsedImages : string[])  : Promise<string[]> => {
+  let data: string[] = [];
+  for (let i = 0; i < parsedImages.length; i++) {
+    try {
+
+    data.push((await downloadImage(parsedImages[i], i.toString())) as string);
+    } catch (e) {
+      console.log(e)
+
+    }
   }
+  return data;
 };
